7c6f22eb55dec688165b7f22d299f27dce492c9e
azizdz12300 <44880483+azizdz12300@users.noreply.github.com>
2019-03-30 12:42:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ Client.login("NTE1OTY0MTk2MDM5NTU3MTQz.D398sg.2LtpZ5q20yDG0UjaKfgikCb2k5Q").then
 })
 Client.on('message', async message => {
     if (message.author.id === '515964196039557143') {
-        let prefix = 'G'
+        let prefix = '!'
         const args = message.content.slice(prefix.length).trim().split(' ') 
         if (message.content.startsWith(`${prefix}avatar`)) {
             Client.user.setAvatar(args[1]).then(async () => {
