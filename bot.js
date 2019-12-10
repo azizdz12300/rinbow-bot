3936da9a6f0bcfb904f03e4e71596021473d6757
azizdz12300 <44880483+azizdz12300@users.noreply.github.com>
2019-12-10 20:22:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,33 +2,21 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
-client.on('message', function(message) {
-    if(!message.channel.guild) return;
-           if (message.content.startsWith(prefix + "colors create")) {
-    if(message.member.hasPermission('MANAGE_ROLES')) {
-    setInterval(function(){})
-    message.channel.send('يتم انشاء 200 لون انتضر | ▶️')
-    }else{
-    message.channel.send('ما معاك البرمشن المطلوب |❌🚫')
-    }
-    }
-    });
-    
-    client.on('message', message=>{
-    if (message.content === 'colors create'){
-    if(!message.channel.guild) return;
-    if (message.member.hasPermission('MANAGE_ROLES')){
-    setInterval(function(){})
-    let count = 0;
-    let ecount = 0;
-    for(let x = 1; x < 200; x++){
-    message.guild.createRole({name:x,
-    color: 'RANDOM'})
-    }
-    }
-    }
-    });
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
+client.on('message', function(message) {
+                  if(!message.channel.guild) return;
+    if(message.content ===  '+setcolors') {
+        if(message.member.hasPermission('MANAGE_ROLES')) {
+            setInterval(function(){})
+            message.channel.send('جاري عمل الالوان يرجى الانتظار لمدة دقيقة |white_check_mark')
+        }else{
+            message.channel.send('ما معاك البرمشن المطلوب  |x')
+            }
+    }
+});
 
 
 
